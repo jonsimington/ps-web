df37a5c0d67c3402ab2ce23ffeb8c89b3e7d8dae
jon <jon@simington.io>
2015-12-06 14:25:38 -0600

    Add number of sentences and complexity to API and perform text_model gen in __main__

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify
-from book_text import book_text
 import nltk
 from nltk.corpus import brown
 import json
-from markovgen import Markov
+import markovify
+import datetime
 
 app = Flask(__name__)
 
@@ -11,16 +11,45 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-@app.route('/<genres>', methods=['GET'])
-def get_sentence(genres):
-    genre_list = genres.split()
+@app.route('/gen-sentence/<number>/<complexity>', methods=['GET'])
+def get_sentence(number, complexity):
+    global liked_sentences
+    global book_text
+    global text_model
 
-    brown_text = [' '.join([x for x in brown.words(categories=genre_list) if x not in ['?', ',', '"', '""', "'", "''", '`', '``', '!']])]
-    markov = Markov(brown_text).generate_markov_text(backward=True)
-    print("markov: ", markov)
-    return markov
+    sentences = []
     
+    for i in range(int(number)):
+        sentences.append(text_model.make_short_sentence(int(complexity)))
+        
+    return json.dumps(sentences)
 
+@app.route('/like/<sentence>', methods=["GET"])
+def like_sentence(sentence):
+    global liked_sentences
+
+    if sentence not in liked_sentences:
+        liked_sentences.append(sentence)
+
+        # really return some code that the client uses to display a thanks popup
+        return liked_sentences[len(liked_sentences) - 1]
+
+    else:
+        return "That one is already liked!"
 
 if __name__ == '__main__':
+    book_file_ids = ['austen-emma.txt', 'austen-persuasion.txt', 'austen-sense.txt',
+                     'blake-poems.txt', 'bryant-stories.txt', 'burgess-busterbrown.txt',
+                     'carroll-alice.txt', 'chesterton-ball.txt', 'chesterton-brown.txt',
+                     'chesterton-thursday.txt', 'edgeworth-parents.txt', 'melville-moby_dick.txt',
+                     'milton-paradise.txt', 'whitman-leaves.txt']
+
+    liked_sentences = [""]
+    book_text = ""
+
+    for book in book_file_ids:
+        book_text += nltk.corpus.gutenberg.raw(book)
+
+    text_model = markovify.Text(book_text)
+
     app.run(debug=True, host='0.0.0.0', port=5000)
